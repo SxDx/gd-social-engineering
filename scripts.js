a3fdaf51ed7fff05e01a7cbd068d48ea6ccfa7db
Rene Koller <r.koller@me.com>
2015-06-22 10:13:30 +0200

    added errors to form, triggered on button press

--- scripts.js
+++ scripts.js
@@ -1,12 +1,18 @@
+$(document).ready(function() { /* off-canvas sidebar toggle */
 
-$(document).ready(function(){/* off-canvas sidebar toggle */
-
-$('[data-toggle=offcanvas]').click(function() {
-  	$(this).toggleClass('visible-xs text-center');
+  $('[data-toggle=offcanvas]').click(function() {
+    $(this).toggleClass('visible-xs text-center');
     $(this).find('i').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
     $('.row-offcanvas').toggleClass('active');
     $('#lg-menu').toggleClass('hidden-xs').toggleClass('visible-xs');
     $('#xs-menu').toggleClass('visible-xs').toggleClass('hidden-xs');
     $('#btnShow').toggle();
-});
+  });
+
+
+  $('#break-in').click(function () {
+    $('.error-time').removeClass('hidden');
+    $('.error-date').removeClass('hidden');
+    $('.error-thing').removeClass('hidden');
+  })
 });
